223eedb3fa2dd56ce0fca14efcbb0378eaf89341
Cole Johnson <cburnjohnson@gmail.com>
2021-07-11 12:45:06 -0700

    imported fonts

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar__brand">K.H.</div>
+    <div class="navbar__menu-icon"></div>
+    <div class="navbar__mobile-menu">
+      <ul class="navbar__media-list">
+        <li class="navbar__media-item"></li>
+        <li class="navbar__media-item"></li>
+        <li class="navbar__media-item"></li>
+      </ul>
+      <ul class="navbar__links">
+        <li class="navbar__link">Events</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
