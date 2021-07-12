92a2346a15e76a45bba1704135012e221c4d6f7f
Cole Johnson <cburnjohnson@gmail.com>
2021-07-11 17:28:31 -0700

    hamburger icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
   <nav class="navbar">
-    <div class="navbar__brand">K.H.</div>
-    <div class="navbar__menu-icon">
-      <span></span>
-      <span></span>
-      <span></span>
+    <div class="navbar__container">
+      <div class="navbar__brand">K.H.</div>
+      <div class="navbar__menu-icon">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
+
     <div class="navbar__mobile-menu">
       <ul class="navbar__media-list">
         <li class="navbar__media-item"><i class="fab fa-instagram"></i></li>
@@ -28,6 +31,12 @@ export default {};
   background-color: $green;
   padding: 1.25rem 2rem;
 
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
   &__brand {
     font-family: $cursive;
     font-size: 2.75rem;
@@ -35,6 +44,40 @@ export default {};
 
   &__mobile-menu {
     display: none;
+  }
+
+  &__menu-icon {
+    width: 30px;
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit-transition: 0.5s ease-in-out;
+    -moz-transition: 0.5s ease-in-out;
+    -o-transition: 0.5s ease-in-out;
+    transition: 0.5s ease-in-out;
+    cursor: pointer;
+
+    span {
+      display: block;
+      height: 5px;
+      width: 100%;
+      background: $white;
+      border-radius: 9px;
+      opacity: 1;
+      -webkit-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      transform: rotate(0deg);
+      -webkit-transition: 0.25s ease-in-out;
+      -moz-transition: 0.25s ease-in-out;
+      -o-transition: 0.25s ease-in-out;
+      transition: 0.25s ease-in-out;
+
+      &:nth-child(2) {
+        margin: 0.25rem 0;
+      }
+    }
   }
 }
 </style>
