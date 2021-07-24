585d1f9526411e4c2178f967e5d35358b2050f27
Cole Johnson <cburnjohnson@gmail.com>
2021-07-24 15:35:20 -0700

    image gallery navbar styling / markup

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -1,16 +1,30 @@
 <template>
   <fragment>
-    <h1 class="title">Image Gallery</h1>
-    <div class="gallery-container">
-      <div class="gallery">
-        <img
-          class="gallery__img"
-          :src="require(`../assets/events/${imgNum}.jpg`)"
-          alt=""
-          v-for="imgNum in imgNums"
-          :key="imgNum"
-          @click="openPopup(require(`../assets/events/${imgNum}.jpg`))"
-        />
+    <div class="events__container">
+      <div class="events__nav">
+        <ul class="events__types">
+          <li class="events__type">Birthday</li>
+          <li class="events__type">Weddings</li>
+        </ul>
+        <ul class="events__names">
+          <li class="events__name">Jimmy's Birthday</li>
+          <li class="events__name">Jimmy's Birthday</li>
+          <li class="events__name">Jimmy's Birthday</li>
+          <li class="events__name">Jimmy's Birthday</li>
+        </ul>
+      </div>
+      <div class="gallery-container">
+        <h1 class="title">Image Gallery</h1>
+        <div class="gallery">
+          <img
+            class="gallery__img"
+            :src="require(`../assets/events/${imgNum}.jpg`)"
+            alt=""
+            v-for="imgNum in imgNums"
+            :key="imgNum"
+            @click="openPopup(require(`../assets/events/${imgNum}.jpg`))"
+          />
+        </div>
       </div>
     </div>
     <div class="popup" ref="popup" @click="closePopup">
@@ -48,6 +62,59 @@ export default {
 </script>
 
 <style lang="scss">
+.events {
+  &__container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    @media ($md-up) {
+      flex-direction: row-reverse;
+    }
+  }
+
+  &__nav {
+    background-color: $green;
+    text-transform: uppercase;
+    font-weight: 700;
+
+    @media ($md-up) {
+      width: 25%;
+    }
+  }
+
+  &__types {
+    display: flex;
+    list-style: none;
+    flex-wrap: wrap;
+    text-align: center;
+    border-bottom: 2px solid $pink;
+    border-top: 2px solid $pink;
+  }
+
+  &__type {
+    width: calc(100% / 3);
+    padding: 0.5rem 0;
+    border-right: 2px solid $pink;
+  }
+
+  &__names {
+    list-style: none;
+    text-align: center;
+    max-height: 110px;
+    overflow-y: auto;
+
+    @media ($md-up) {
+      max-height: 100%;
+    }
+  }
+
+  &__name {
+    padding: 0.75rem 0;
+    border-bottom: 1px solid $pink;
+  }
+}
+
 .title {
   color: $pink;
   font-family: $cursive;
@@ -59,11 +126,12 @@ export default {
 .gallery-container {
   max-height: 100%;
   overflow-y: auto;
-  max-width: 80%;
   margin: 0 auto;
+  width: 80%;
 
   @media ($md-up) {
     max-width: 90%;
+    width: 70%;
   }
 }
 
@@ -83,6 +151,7 @@ export default {
 
   &__img {
     max-width: 100%;
+    cursor: pointer;
   }
 }
 
