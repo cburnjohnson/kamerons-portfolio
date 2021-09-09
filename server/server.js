86c6c6128c66ca9a6d4defd8e62936ea47e4d11f
Cole Johnson <cburnjohnson@gmail.com>
2021-09-08 17:37:34 -0700

    serve vue app

--- server/server.js
+++ server/server.js
@@ -184,6 +184,16 @@ app.delete('/files/:id', (req, res) => {
   });
 });
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/dist'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+}
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
